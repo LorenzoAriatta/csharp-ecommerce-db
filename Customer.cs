5cf5fd8a19b933b9be0935bac82d050d3357ab2d
Lorenzo Ariatta <lorenzotk77@gmail.com>
2022-07-12 15:27:13 +0200

    add Custumer class EF

---
+++ Customer.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+[Table("student")]
+public class Customer
+{
+    [Key]
+    public int CustomerId { get; set; }
+
+    [Required]
+    public string Name { get; set; }
+
+    public string Surname { get; set; }
+
+    [Column("customer_email")]
+    public string Email { get; set; }
+}
